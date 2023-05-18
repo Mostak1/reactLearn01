c8e6adb99bef5d63899200acab5773a8c072250f
Mostak1 <mostakidb@gmail.com>
2023-05-18 19:03:05 +0600

    'cls'

--- src/pages/clswork/Props.js
+++ src/pages/clswork/Props.js
@@ -3,13 +3,13 @@ import React from 'react'
 export const Props = () => {
   return (
     <>
-    Props
-    <Car brand="Ford" />
+    <h2 className='text-center'>Props Content Here</h2>
+    <Car brand="Food" />
     </>
   )
 }
-function Car({}) {
-  return <h2>I likes masala { {brand} }!</h2>;
+function Car({brand}) {
+  return <h2>I likes masala { brand }!</h2>;
 }
 function Car1(props) {
   return <h2>I am a { props.brand.model } !</h2>;
